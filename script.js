3713598aa67996b2085ec696653f8ced22c93197
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-10 17:30:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+const select = document.getElementById("colorSelect") ;
+const selectedValue = select.value ;
+const button = document.querySelector("input[type=button]") ;
+const child = selectedValue.children ;
+
+function delete() {
+	for(let i=0; i< child.length ; i++) {
+		if(selectedValue === child[i].value) {
+			child[i].remove();
+		}
+	}
+}
+button.eventListener("click" , delete()) ;
